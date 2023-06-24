458d9474a0261e3e832f5a81bd20deef6c24a004
Michael <michael.alexander0008@gmail.com>
2023-06-24 15:43:41 +0100

    Add ability to edit books via the webpage

--- script.js
+++ script.js
@@ -123,22 +123,30 @@ function accessBookLibrary(bookID) {
   }
 }
 
+function editBookLibrary(bookID, title, author, numPages, pagesRead) {
+  const book = accessBookLibrary(bookID);
+  book.setTitle(title);
+  book.setAuthor(author);
+  book.setNumPages(numPages);
+  book.setPagesRead(pagesRead);
+}
+
 // DOM functions
 
-function createBookDOMElement(book) {
-  function updateProgressIcon(book, progressIcon) {
-    if (book.progressStatus === 0) {
-      progressIcon.setAttribute('src', './img/icons/book-not-started.svg');
-      progressIcon.setAttribute('alt', 'Icon of an unopened book');
-    } else if (book.progressStatus === 100) {
-      progressIcon.setAttribute('src', './img/icons/read-book.png');
-      progressIcon.setAttribute('alt', 'Icon of a finished book');
-    } else {
-      progressIcon.setAttribute('src', './img/icons/book-in-progress.svg');
-      progressIcon.setAttribute('alt', 'Icon of a book in progress');
-    }
-  } 
+function updateProgressIcon(book, progressIcon) {
+  if (book.progressStatus === 0) {
+    progressIcon.setAttribute('src', './img/icons/book-not-started.svg');
+    progressIcon.setAttribute('alt', 'Icon of an unopened book');
+  } else if (book.progressStatus === 100) {
+    progressIcon.setAttribute('src', './img/icons/read-book.png');
+    progressIcon.setAttribute('alt', 'Icon of a finished book');
+  } else {
+    progressIcon.setAttribute('src', './img/icons/book-in-progress.svg');
+    progressIcon.setAttribute('alt', 'Icon of a book in progress');
+  }
+} 
 
+function createBookDOMElement(book) {
   const bookElement = createElement('div', {"class": ["book-card"], "data-book-id": book.getBookID().toString()});
 
   bookElement.appendChild(createElement('div', {"class": ["progress-background"], 'style': 'width: ' + book.getProgressStatus() + '%;'}));
@@ -174,10 +182,12 @@ function createBookDOMElement(book) {
   const options = createElement('div', {"class": ['options']});
   
   const progressIcon = createElement('img');
+  progressIcon.classList.add('progress-icon');
   updateProgressIcon(book, progressIcon);
   const editIcon = createElement('img');
   editIcon.setAttribute('src', './img/icons/edit.svg');
   editIcon.setAttribute('alt', 'Edit icon');
+  editIcon.addEventListener('click', displayEditBookForm);
   const deleteIcon = createElement('img');
   deleteIcon.setAttribute('src', './img/icons/delete-book.svg');
   deleteIcon.setAttribute('alt', 'Delete book icon');
@@ -200,18 +210,60 @@ function displayBook(book) {
   main.insertBefore(bookDOMElement, main.lastElementChild);
 }
 
+function editBookDOM(bookID, title, author, numPages, pagesRead) {
+  let bookCard = document.querySelector(`.book-card[data-book-id="${bookID}"]`);
+  bookCard.querySelector('.title').textContent = title;
+  bookCard.querySelector('.author').textContent = author;
+  bookCard.querySelector('.pages p').textContent = `${pagesRead} / ${numPages}`;
+
+  let book = accessBookLibrary(bookID);
+  const progressIcon = bookCard.querySelector('.options .progress-icon')
+  updateProgressIcon(book, progressIcon);
+  bookCard.querySelector('.progress-background').setAttribute('style', `width: ${book.progressStatus}%`);
+}
+
 function displayAddBookForm() {
   const mainContentContainer = document.getElementById('main-content-container');
   const popupContainer = document.getElementById('popup-container');
   clearAddEditForm();
+  addEditFormDisplayAddText();
   normalValidationBehaviourAddEditForm();
+
+  const addEditForm = document.getElementById('add-edit-book');
+  addEditForm.setAttribute('data-book-id', 'na');
+  addEditForm.setAttribute('data-mode', 'add');
 
   mainContentContainer.setAttribute('style', 'filter: blur(3px);')
   popupContainer.setAttribute('style', 'z-index: 9999; display: block;')
   document.querySelector('body').setAttribute('style', 'overflow-y: hidden;')
 }
 
-function closeAddBookForm() {
+function displayEditBookForm() {
+  const mainContentContainer = document.getElementById('main-content-container');
+  const popupContainer = document.getElementById('popup-container');
+  clearAddEditForm();
+  addEditFormDisplayEditText();
+  normalValidationBehaviourAddEditForm();
+
+  let bookCard = this;
+  while (!bookCard.hasAttribute('data-book-id')) {
+    bookCard = bookCard.parentNode;
+  }
+  const bookID = Number(bookCard.getAttribute('data-book-id'));
+  const book = accessBookLibrary(bookID);
+
+  prepopulateEditForm(book);
+
+  const addEditForm = document.getElementById('add-edit-book');
+  addEditForm.setAttribute('data-book-id', bookID.toString());
+  addEditForm.setAttribute('data-mode', 'edit');
+
+  mainContentContainer.setAttribute('style', 'filter: blur(3px);')
+  popupContainer.setAttribute('style', 'z-index: 9999; display: block;')
+  document.querySelector('body').setAttribute('style', 'overflow-y: hidden;')
+}
+
+function closeAddEditBookForm() {
   const mainContentContainer = document.getElementById('main-content-container');
   const popupContainer = document.getElementById('popup-container');
 
@@ -323,6 +375,30 @@ function clearAddEditForm() {
   }
 }
 
+function addEditFormDisplayAddText() {
+  const addEditFormHeader = document.querySelector("#add-edit-book h2");
+  addEditFormHeader.textContent = "Add Book";
+
+  const addEditFormSubmitButton = document.querySelector('#add-edit-book button[type="submit"]')
+  addEditFormSubmitButton.textContent = "Add Book";
+}
+
+function addEditFormDisplayEditText() {
+  const addEditFormHeader = document.querySelector("#add-edit-book h2");
+  addEditFormHeader.textContent = "Edit Book";
+
+  const addEditFormSubmitButton = document.querySelector('#add-edit-book button[type="submit"]')
+  addEditFormSubmitButton.textContent = "Edit Book";
+}
+
+function prepopulateEditForm(book) {
+  let inputs = document.querySelectorAll('#add-edit-book .input-textual-container input');
+  for (let i = 0; i < inputs.length; i++) {
+    let input = inputs.item(i);
+    input.value = book[`get${capitalizeString(input.getAttribute('id'))}`]();
+  }
+}
+
 function normalValidationBehaviourAddEditForm() {
   document.getElementById('title').removeEventListener('input', validateOninput);
 
@@ -368,8 +444,13 @@ function validateAddEditForm(e) {
     failedSubmitValidationBehaviourAddEditForm();
   } else {
     e.preventDefault();
-    addBook(title.value, author.value, Number(numPages.value), Number(pagesRead.value));
-    closeAddBookForm();
+    if (addEditForm.getAttribute('data-mode') === 'add') {
+      addBook(title.value, author.value, Number(numPages.value), Number(pagesRead.value));
+    } else {
+      const bookID = Number(addEditForm.getAttribute('data-book-id'));
+      editBook(bookID, title.value, author.value, Number(numPages.value), Number(pagesRead.value));
+    }
+    closeAddEditBookForm();
   }
 }
 
@@ -393,6 +474,11 @@ function removeBook(e) {
   removeBookLibrary(bookID);
 }
 
+function editBook(bookID, title, author, numPages, pagesRead) {
+  editBookLibrary(bookID, title, author, numPages, pagesRead);
+  editBookDOM(bookID, title, author, numPages, pagesRead);
+}
+
 // Utility functions
 
 function createElement(type, attributes = {}) {
@@ -410,11 +496,21 @@ function createElement(type, attributes = {}) {
   return element;
 }
 
+function capitalizeString(str) {
+  if (str.length === 0) {
+    return str;
+  } else if (str.length === 1) {
+    return str.charAt(0).toUpperCase();
+  } else {
+    return str.charAt(0).toUpperCase() + str.substring(1, str.length);
+  }
+}
+
 // Page initialization
 
 function initializePage() {
   document.querySelector('.add-book-button').addEventListener('click', displayAddBookForm);
-  document.getElementById('close-add-edit-book').addEventListener('click', closeAddBookForm);
+  document.getElementById('close-add-edit-book').addEventListener('click', closeAddEditBookForm);
   document.getElementById('add-edit-book').addEventListener("submit", validateAddEditForm);
 }
 
